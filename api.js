8a5c2d9bf34e2c6056b6b1297d20a19620d2c40c
ailuni12 <ailunicliz@gmail.com>
2022-10-25 21:01:05 -0300

    Añadida nueva API, cambios de estilo en algunas partes

---
+++ api.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        info: {},
+        datetime: ""
+    },
+    created() {
+        fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires.json')
+        .then(response => response.json())
+        .then(data => { 
+            console.log(data);
+            this.datetime=data.datetime.substr(11,11)
+            })
+        .catch( error => console.log(error));
+    }
+});
